d609a2092e6c5bbb71279bd1fb2077c1e82f0fb8
Md Waseem Akram <mdwaseem0603@gmail.com>
2023-11-04 03:19:16 +0530

    ...

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
     name: {
       type: String,
       required: true,
-      max: [25, "Max Length should be 25"],
+      maxlength: [25, "Max Length should be 25"],
     },
     avatar: { type: String, required: true },
     email: {
